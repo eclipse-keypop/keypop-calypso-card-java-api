3d330b6de8519d8f549f89a8148714585e91e20a
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2024-02-16 18:02:16 +0100

    feat: add CardCertificate interface

---
+++ src/main/java/org/eclipse/keypop/calypso/card/transaction/spi/CardCertificate.java
@@ -0,0 +1,20 @@
+/* ******************************************************************************
+ * Copyright (c) 2024 Calypso Networks Association https://calypsonet.org/
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License which is available at
+ * https://opensource.org/licenses/MIT.
+ *
+ * SPDX-License-Identifier: MIT
+ ****************************************************************************** */
+package org.eclipse.keypop.calypso.card.transaction.spi;
+
+/**
+ * Marker interface for card certificate.
+ *
+ * <p>The implementation of this interface is provided by the Keypop extensions for asymmetric
+ * cryptography.
+ *
+ * @since 2.1.0
+ */
+public interface CardCertificate {}
